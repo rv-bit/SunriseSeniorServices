b670bd588a780710e3e762a9a0b0121dd118ad4c
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-02-23 21:47:04 +0000

    Added dummy data to understand and see if api works and pulls correct data

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from 'react';
 import {config} from '../common/config.js'
 
 export default function Login () {
+  const [data, setData] = useState(null)
+
+  // useEffect(() => {
+  //   fetch(`${config.apiPrefix}/chat`)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       setData(data) 
+  //       console.log(data)
+  //     })
+  //     .catch(error => console.error('Error fetching data:', error));
+  // }, []);
+
   return (
     <div>
       <h1>Login</h1>
@@ -9,6 +22,19 @@ export default function Login () {
         <input type='password' placeholder='Password' />
         <button type='submit'>Login</button>
       </form>
+
+      {data && (
+        <div>
+          {Object.values(data).map((value, index) => {
+            return (
+              <div key={index}>
+                <h2>{value}</h2>
+                <hr />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   )
 }
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from backend import initializeApp
-from flask_cors import CORS  # comment this on deployment
-
 app = initializeApp()
-CORS(app)  # comment this on deployment
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
