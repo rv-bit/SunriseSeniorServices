a5a269a4f45619958ea97b05d2ac3a773eaa10a0
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-02-23 15:41:02 +0000

    Add config file and App component with API integration

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import { apiPrefix } from '../common/config'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch(`${apiPrefix}/chat`)
+      .then(response => response.json())
+      .then(data => {
+        setData(data) 
+        console.log(data)
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (  
+    <div>
+      <p1>
+        Hello, World! This is a React app. The count is {count}.
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+
+        {data && (
+          <div>
+            {Object.values(data).map((value, index) => {
+              return (
+                <div key={index}>
+                  <h2>{value}</h2>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </p1>
+    </div>
+  )
+}
+
+export default App
