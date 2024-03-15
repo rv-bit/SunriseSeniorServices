e90cae5369f8a3a852349c48c397aad69380f76a
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-03-15 21:33:45 +0000

    Refactor catch_all route and error handler
    This commit refactors the catch_all route and error handler in both `views.py` and `main.py` files. The code has been simplified and made more consistent.

--- backend/views.py
+++ backend/views.py
@@ -6,6 +6,17 @@ from flask_login import current_user
 views = Blueprint('views', __name__)
 
 
+@views.route('/', defaults={'path': ''})
+@views.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
+
+
+@views.errorhandler(404)
+def not_found(e):
+    return render_template('index.html')
+
+
 @views.route("/", methods=["GET"])
 def home():
     if not current_user.is_authenticated:
--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-from flask import render_template
 from backend import initializeApp
 from flask_restful import Api
 
 app = initializeApp()
 api = Api(app)
-
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return render_template("index.html")
-
-
-@app.errorhandler(404)
-def not_found(e):
-    return render_template('index.html')
 
 
 if __name__ == "__main__":
