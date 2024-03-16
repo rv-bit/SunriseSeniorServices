0f366ef87ab5d09dc5e4a85dc5b9d77103f1b6b7
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-03-16 22:22:53 +0000

    Refactor home route in views.py

--- backend/views.py
+++ backend/views.py
@@ -5,19 +5,13 @@ from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
-if os.environ.get("NODE_ENV") == "production":
-    @views.route('/', defaults={'path': ''})
-    @views.route('/<path:path>')
-    def catch_all(path):
-        return render_template("index.html")
 
-
-@views.route("/", methods=["GET"])
+@views.route("/home", methods=["GET"])
 def home():
-    if request.method == "GET":
-        if current_user.is_authenticated:
-            return jsonify({"user": current_user.get_user_info()}), 200
-
+    if not current_user.is_authenticated:
         return jsonify({"user": "Anonymous"}), 200
 
-    return {}, 200
+    if request.method == "GET":
+        return jsonify({"user": current_user.get_user_info()}), 200
+
+    return render_template('index.html'), 200
