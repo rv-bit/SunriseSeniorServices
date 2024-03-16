dac9e32bd15f82ff5d09c2c8b5c0ed970e67d6c9
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-03-16 17:14:50 +0000

    Add error handling for 404 response

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ from flask_restful import Api
 app = initializeApp()
 api = Api(app)
 
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+
 if __name__ == "__main__":
     app.run()
