4c7df46982c6914f0ce1ae19157b46c2cfa6c1f7
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-03-16 22:23:46 +0000

    Add catch-all route and error handler for production environment

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os
+
+from flask import render_template
 from backend import initializeApp
 app = initializeApp()
+
+
+if os.environ.get("NODE_ENV") == "production":
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def catch_all(path):
+        return render_template("index.html")
+
+    @app.errorhandler(404)
+    def not_found(e):
+        return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
