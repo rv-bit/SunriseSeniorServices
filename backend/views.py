3aa3ed624f14e8ed651e7f02f728feae7249f53e
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-03-16 01:38:09 +0000

    Add index redirect route and remove unnecessary code

--- backend/views.py
+++ backend/views.py
@@ -4,6 +4,12 @@ from flask_login import current_user
 views = Blueprint('views', __name__)
 
 
+@views.route('/')
+def index_redir():
+    # Reached if the user hits example.com/ instead of example.com/index.html
+    return render_template('index.html')
+
+
 @views.route("/", methods=["GET"])
 def home():
     if not current_user.is_authenticated:
--- main.py
+++ main.py
@@ -6,11 +6,5 @@ from flask_restful import Api
 app = initializeApp()
 api = Api(app)
 
-if os.environ.get("NODE_ENV") == "production":
-    @app.errorhandler(404)
-    def not_found(e):
-        return app.send_static_file('index.html')
-
-
 if __name__ == "__main__":
     app.run()
