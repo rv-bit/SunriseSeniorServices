50870d1637a1e317e267fb6e365436c22d045c05
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-03-16 22:23:30 +0000

    Add catch-all route for production environment

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import render_template
 
 from backend import initializeApp
@@ -6,16 +8,15 @@ from flask_restful import Api
 app = initializeApp()
 api = Api(app)
 
+if os.environ.get("NODE_ENV") == "production":
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def catch_all(path):
+        return render_template("index.html")
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return render_template("index.html")
-
-
-@app.errorhandler(404)
-def not_found(e):
-    return app.send_static_file('index.html')
+    @app.errorhandler(404)
+    def not_found(e):
+        return app.send_static_file('index.html')
 
 
 if __name__ == "__main__":
