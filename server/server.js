47e2777c46e8be79c0b62902e6be4e674bafe19d
Robert Vilcu <robertvilcu89@gmail.com>
2024-05-02 21:31:02 +0100

    updated: catch all to try and fix the MME error

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use('/user', userRouter);
 app.use('/chats', chatRouter);
 
 if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "docker") {
-    app.get('/*', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
     });
 }
